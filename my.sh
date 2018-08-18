4dd0640c1e856fc93895dab1483c795669a142ba
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2018-08-17 20:35:22 -0400

    Update my.sh

--- my.sh
+++ my.sh
@@ -6,6 +6,6 @@
 if [[ "/" = "${0:0:1}" ]]; then    echo # "Fully Qualified Path: $0"
 else  	echo "** Used Relative Path: $0 -- Must use Fully Qualified Path **";  	exit;
 fi
-export Srv=`dirname $0` ;	 # echo "export Srv= >> $Srv >>   `dirname $0`"
+export Srv=`dirname $0` ;	  echo "export Srv= >> $Srv >>   `dirname $0`"
 env -i LS_COLORS=$LS_COLORS  PATH=$Srv/bin:$PATH  PWD=$PWD Srv=$Srv  TERM=xterm \
 	$Srv/myEnv.sh $@
