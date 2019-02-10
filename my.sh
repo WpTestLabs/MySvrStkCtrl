6818773162f90cf6fb3337ae17964ea5c5d24711
WpTestLabs <37196239+WpTestLabs@users.noreply.github.com>
2019-02-10 09:54:25 -0500

    Update my.sh

--- my.sh
+++ my.sh
@@ -8,4 +8,5 @@ else  	echo "** Used Relative Path: $0 -- Must use Fully Qualified Path **";  	e
 fi
 export Srv=`dirname $0` ;	  echo "export Srv= >> $Srv >>   `dirname $0`"
 env -i LS_COLORS=$LS_COLORS  PATH=$Srv/bin:$PATH  PWD=$PWD Srv=$Srv  TERM=xterm \
-	$Srv/myEnv.sh $@
+    GoDevPth=$GoDevPth \
+  $Srv/myEnv.sh $@
